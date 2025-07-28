1050006609cca451a987fab0db9875466ceb93dc
miyazaku <solomiyazaku@icloud.com>
2025-07-28 20:42:49 +0200

    add all

--- js/task-3.js
+++ js/task-3.js
@@ -1,23 +1,17 @@
-function getElementWidth(content, padding, border) {
-    const message = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
-  return message;
+function filterArray(numbers, value) {
+  const total = [];
+  for ( let i = 0; i < numbers.length; i++ ) {
+      if (numbers[i] > value) {
+           total.push(numbers[i]);
+      } 
+  }
+
+  return total;
 }
 
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-function checkForSpam(message) {
-  const lowerCaseMessage = message.toLowerCase();
-  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
-}
-
-
-console.log(checkForSpam("Latest technology news"));                      // false
-console.log(checkForSpam("JavaScript weekly newsletter"));               // false
-console.log(checkForSpam("Get best sale offers now!"));                  // true
-console.log(checkForSpam("Amazing SalE, only tonight!"));                // true
-console.log(checkForSpam("Trust me, this is not a spam message"));       // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));// true
-console.log(checkForSpam("[SPAM] How to earn fast money?"));             // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
